08c47091217f6b879bb2d30d77c14b5dd4280eb8
Cristian Tabares <tabares9209@gmail.com>
2023-01-15 16:32:30 -0500

    Carga inicial proyecto

---
+++ appbitacoras/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    id_area = models.CharField(max_length=40)
+
+class bitacora(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    numerobit = models.CharField(max_length=40)
+    asignedto = models.CharField(max_length=50)
+    dateini = models.DateField(auto_now_add=True)
+    datefin = models.DateField()
+    compañia = models.CharField(max_length=40)
+
+
+class area(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    proceso = models.CharField(max_length=80)
+    servicio = models.CharField(max_length=80)
+    ans = models.IntegerField()
+
+
+
+
